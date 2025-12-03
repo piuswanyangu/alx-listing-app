bea54f99c6464ba600fc0dd78b94bc2b2d1b5223
pius wanyangu <ndubipius96@gmail.com>
2025-12-04 00:27:53 +0300

    responsive listing page

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,26 @@ export interface ButtonProps {
     label: string;
     onclick?: ()=> void
 }
+// address interface
+export interface Address {
+    state: string;
+    city: string;
+    country: string;
+}
+// offers interface
+export interface Offers {
+    bed: string;
+    shower: string;
+    occupants: string;
+}
+//  property props
+export interface PropertyProps {
+    name: string;
+    address: Address;
+    rating: number;
+    category: string[];
+    price: number;
+    offers: Offers;
+    image: string;
+    discount?: string;
+}
